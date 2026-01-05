87ed423c9d251b87ffbe5ce97d44702baf89122a
MarioAlexandru1 <popescu.mario.r7u@student.ucv.ro>
2026-01-05 18:06:11 +0200

    Implementare meniu functiile de baza ( afisare si adaugare vehicul)

--- FlotaTransport/FlotaTransport.cpp
+++ FlotaTransport/FlotaTransport.cpp
@@ -7,30 +7,75 @@
 
 using namespace std;
 
+void afiseazaMeniu() {
+    cout << "\n=== MENIU FLOTA TRANSPORT ===\n";
+    cout << "1. Adauga vehicul\n";
+    cout << "2. Afiseaza flota\n";
+    cout << "0. Iesire\n";
+    cout << "Alege optiunea: ";
+}
+
 int main() {
-    cout << "=== Proiect: Flota de transport ===\n";
-
     Flota flota;
+    int optiune;
 
-    // Adaugam vehicule folosind clasele derivate
-    flota.adaugaVehicul(
-        make_shared<Autobuz>("A01", "Mercedes", 50, 24)
-    );
+    do {
+        afiseazaMeniu();
+        cin >> optiune;
 
-    flota.adaugaVehicul(
-        make_shared<Tren>("T01", "Siemens", 300, "Bucuresti-Cluj")
-    );
+        if (optiune == 1) {
+            int tip;
+            cout << "\nTip vehicul:\n";
+            cout << "1. Autobuz\n";
+            cout << "2. Tren\n";
+            cout << "3. Avion\n";
+            cout << "Alege: ";
+            cin >> tip;
 
-    flota.adaugaVehicul(
-        make_shared<Avion>("V01", "Boeing", 180, "OTP", "JFK")
-    );
+            string id, marca;
+            int capacitate;
 
-    // Afisam flota
-    flota.afiseazaFlota();
+            cout << "ID: ";
+            cin >> id;
+            cout << "Marca: ";
+            cin >> marca;
+            cout << "Capacitate: ";
+            cin >> capacitate;
 
-    cout << "\nNumar total de vehicule: "
-        << flota.getNumarVehicule() << "\n";
+            if (tip == 1) {
+                int linie;
+                cout << "Linie autobuz: ";
+                cin >> linie;
 
-    cout << "\nProgram incheiat.\n";
+                flota.adaugaVehicul(make_shared<Autobuz>(id, marca, capacitate, linie));
+            }
+            else if (tip == 2) {
+                string ruta;
+                cout << "Ruta tren: ";
+                cin.ignore();
+                getline(cin, ruta);
+
+                flota.adaugaVehicul(make_shared<Tren>(id, marca, capacitate, ruta));
+            }
+            else if (tip == 3) {
+                string plecare, destinatie;
+                cout << "Aeroport plecare: ";
+                cin >> plecare;
+                cout << "Aeroport destinatie: ";
+                cin >> destinatie;
+
+                flota.adaugaVehicul(
+                    make_shared<Avion>(id, marca, capacitate, plecare, destinatie)
+                );
+            }
+        }
+        else if (optiune == 2) {
+            flota.afiseazaFlota();
+            cout << "Numar total vehicule: " << flota.getNumarVehicule() << "\n";
+        }
+
+    } while (optiune != 0);
+
+    cout << "Program incheiat.\n";
     return 0;
 }
