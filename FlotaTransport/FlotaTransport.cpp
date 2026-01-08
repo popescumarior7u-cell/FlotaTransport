af7fb5bac3014d65aa9da11e47cae9d8ff99ed70
MarioAlexandru1 <popescu.mario.r7u@student.ucv.ro>
2026-01-08 21:32:46 +0200

    Freed up main Mbappe

--- FlotaTransport/FlotaTransport.cpp
+++ FlotaTransport/FlotaTransport.cpp
@@ -9,7 +9,14 @@
 
 using namespace std;
 
-void afiseazaMeniu() {
+void citesteDinFisier(const string& numeFisier, Flota& flota);
+
+void afiseazaMeniu(int &optiune) {
+
+    Flota flota;
+
+    citesteDinFisier("date_intrare.txt", flota);
+
     cout << "\n=== MENIU FLOTA TRANSPORT ===\n";
     cout << "1. Adauga vehicul\n";
     cout << "2. Afiseaza flota\n";
@@ -20,6 +27,136 @@ void afiseazaMeniu() {
     cout << "7. Gaseste ruta intre 2 statii\n";
     cout << "0. Iesire\n";
     cout << "Alege optiunea: ";
+
+    cin >> optiune;
+
+    if (optiune == 1) {
+        int tip;
+        cout << "\nTip vehicul:\n";
+        cout << "1. Autobuz\n";
+        cout << "2. Tren\n";
+        cout << "3. Avion\n";
+        cout << "Alege: ";
+        cin >> tip;
+
+        string id, marca;
+        int capacitate;
+
+        cout << "ID: ";
+        cin >> id;
+        cout << "Marca: ";
+        cin >> marca;
+        cout << "Capacitate: ";
+        cin >> capacitate;
+
+        if (tip == 1) {
+            int linie;
+            cout << "Linie autobuz: ";
+            cin >> linie;
+
+            int nrStatii;
+            cout << "Numar statii pe ruta: ";
+            cin >> nrStatii;
+
+            vector<string> statii;
+            for (int i = 0; i < nrStatii; i++) {
+                string s;
+                cout << "Statie " << i + 1 << ": ";
+                cin >> s;
+                statii.push_back(s);
+            }
+
+            Ruta ruta(statii);
+
+            flota.adaugaVehicul(
+                make_shared<Autobuz>(id, marca, capacitate, linie, ruta)
+            );
+
+        }
+        else if (tip == 2) {
+            int nrStatii;
+            cout << "Numar statii pe ruta: ";
+            cin >> nrStatii;
+
+            vector<string> statii;
+            for (int i = 0; i < nrStatii; i++) {
+                string s;
+                cout << "Statie " << i + 1 << ": ";
+                cin >> s;
+                statii.push_back(s);
+            }
+
+            Ruta ruta(statii);
+
+            flota.adaugaVehicul(
+                make_shared<Tren>(id, marca, capacitate, ruta)
+            );
+        }
+        else if (tip == 3) {
+            string plecare, destinatie;
+            cout << "Aeroport plecare: ";
+            cin >> plecare;
+            cout << "Aeroport destinatie: ";
+            cin >> destinatie;
+
+            flota.adaugaVehicul(
+                make_shared<Avion>(id, marca, capacitate, plecare, destinatie)
+            );
+        }
+    }
+    else if (optiune == 2) {
+        flota.afiseazaFlota();
+        cout << "Numar total vehicule: " << flota.getNumarVehicule() << "\n";
+    }
+    else if (optiune == 3) {
+        string id;
+        cout << "Introdu ID vehicul de sters: ";
+        cin >> id;
+
+        if (flota.stergeVehicul(id))
+            cout << "Vehicul sters cu succes.\n";
+        else
+            cout << "Vehiculul nu a fost gasit.\n";
+    }
+    else if (optiune == 4) {
+        string tip;
+        cout << "Introdu tipul vehiculului (Autobuz / Tren / Avion): ";
+        cin >> tip;
+
+        flota.cautaDupaTip(tip);
+    }
+    else if (optiune == 5)
+    {
+        int cap;
+        cout << "Introdu capacitatea minima: ";
+        cin >> cap;
+
+        flota.filtreazaDupaCapacitate(cap);
+    }
+    else if (optiune == 6)
+    {
+        string id;
+        cout << "Introdu ID-ul vehiculului: ";
+        cin >> id;
+
+        flota.actualizeazaStareVehicul(id);
+
+    }
+    else if (optiune == 7)
+    {
+        string start, stop;
+
+        cout << "Statie plecare: ";
+        cin >> start;
+
+        cout << "Statie destinatie: ";
+        cin >> stop;
+
+        flota.gasesteRuta(start, stop);
+
+    }
+
+
 }
 void citesteDinFisier(const string& numeFisier, Flota& flota) {
     ifstream fin(numeFisier);
@@ -83,143 +220,13 @@ void citesteDinFisier(const string& numeFisier, Flota& flota) {
 
 
 int main() {
-    Flota flota;
-
-    citesteDinFisier("date_intrare.txt", flota);
+  
 
     int optiune;
 
     do {
-        afiseazaMeniu();
-        cin >> optiune;
-
-        if (optiune == 1) {
-            int tip;
-            cout << "\nTip vehicul:\n";
-            cout << "1. Autobuz\n";
-            cout << "2. Tren\n";
-            cout << "3. Avion\n";
-            cout << "Alege: ";
-            cin >> tip;
-
-            string id, marca;
-            int capacitate;
-
-            cout << "ID: ";
-            cin >> id;
-            cout << "Marca: ";
-            cin >> marca;
-            cout << "Capacitate: ";
-            cin >> capacitate;
-
-            if (tip == 1) {
-                int linie;
-                cout << "Linie autobuz: ";
-                cin >> linie;
-
-                int nrStatii;
-                cout << "Numar statii pe ruta: ";
-                cin >> nrStatii;
-
-                vector<string> statii;
-                for (int i = 0; i < nrStatii; i++) {
-                    string s;
-                    cout << "Statie " << i + 1 << ": ";
-                    cin >> s;
-                    statii.push_back(s);
-                }
-
-                Ruta ruta(statii);
-
-                flota.adaugaVehicul(
-                    make_shared<Autobuz>(id, marca, capacitate, linie, ruta)
-                );
-
-            }
-            else if (tip == 2) {
-                int nrStatii;
-                cout << "Numar statii pe ruta: ";
-                cin >> nrStatii;
-
-                vector<string> statii;
-                for (int i = 0; i < nrStatii; i++) {
-                    string s;
-                    cout << "Statie " << i + 1 << ": ";
-                    cin >> s;
-                    statii.push_back(s);
-                }
-
-                Ruta ruta(statii);
-
-                flota.adaugaVehicul(
-                    make_shared<Tren>(id, marca, capacitate, ruta)
-                );
-            }
-            else if (tip == 3) {
-                string plecare, destinatie;
-                cout << "Aeroport plecare: ";
-                cin >> plecare;
-                cout << "Aeroport destinatie: ";
-                cin >> destinatie;
-
-                flota.adaugaVehicul(
-                    make_shared<Avion>(id, marca, capacitate, plecare, destinatie)
-                );
-            }
-        }
-        else if (optiune == 2) {
-            flota.afiseazaFlota();
-            cout << "Numar total vehicule: " << flota.getNumarVehicule() << "\n";
-        }
-        else if (optiune == 3) {
-            string id;
-            cout << "Introdu ID vehicul de sters: ";
-            cin >> id;
-
-            if (flota.stergeVehicul(id))
-                cout << "Vehicul sters cu succes.\n";
-            else
-                cout << "Vehiculul nu a fost gasit.\n";
-        }
-        else if (optiune == 4) {
-            string tip;
-            cout << "Introdu tipul vehiculului (Autobuz / Tren / Avion): ";
-            cin >> tip;
-
-            flota.cautaDupaTip(tip);
-        }
-         else if (optiune == 5)
-            {
-            int cap;
-            cout << "Introdu capacitatea minima: ";
-            cin >> cap;
-
-            flota.filtreazaDupaCapacitate(cap);
-             }
-              else if (optiune == 6)
-                 {
-                     string id;
-                     cout << "Introdu ID-ul vehiculului: ";
-                     cin >> id;
-
-                     flota.actualizeazaStareVehicul(id);
-             
-                 }
-              else if (optiune == 7)
-                  {
-                      string start, stop;
-
-                      cout << "Statie plecare: ";
-                      cin >> start;
-
-                      cout << "Statie destinatie: ";
-                      cin >> stop;
-
-                      flota.gasesteRuta(start, stop);
-
-                  }
-
-         
+        afiseazaMeniu(optiune);
+        
 
 
     } while (optiune != 0);
